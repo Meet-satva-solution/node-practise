47d3e8038f95181c64f7a45f1a9b2a0f863e5d7d
Meet Patel <meetpatel01997@gmail.com>
2023-06-14 11:37:19 +0530

    added filter sort by

--- Controller/userController.ts
+++ Controller/userController.ts
@@ -1,48 +1,31 @@
 import { Request, Response } from "express";
-import User from "../Schema/userSchema";
 import { ResponseModel } from "../Model";
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
 const saveUser = async (req: Request, res: Response) => {
-  // const { name, email, password } = req.body;
-  // const nameU = req.body.name;
-  const user = new User(req.body)
   try {
-    // let userObj = {
-    //   email: email,
-    //   name: name,
-    //   password: password,
-    // };
-    // let userObjs = new User()
-    // userObjs.email = email
-    // userObjs.name = name
-    // userObjs.password = password
-    // let resp= await user.save();
-    const newUser =  await prisma.userlist.create({
+    const newUser = await prisma.userlist.create({
       data: {
-        name : req.body.name,
-        email : req.body.email
-      }
-    })
+        name: req.body.name,
+        email: req.body.email,
+      },
+    });
 
-    const findUser = await prisma.userlist.findMany({
-      where : {
-        name : {
-          contains: "Mee"
-        }
-      }
-    })
-
-
-    // let userData = await User.create(userObj);
+    // const findUser = await prisma.userlist.findMany({
+    //   where: {
+    //     name: {
+    //       contains: "Mee",
+    //     },
+    //   },
+    // });
     let response = new ResponseModel();
-    response.data = findUser
+    response.data = newUser;
     response.status = 201;
     response.message = "User created successfully";
     res.status(201).json(response);
-  } catch (error:any) {
+  } catch (error: any) {
     let response = new ResponseModel();
     response.data = null;
     response.status = 400;
@@ -53,32 +36,28 @@ const saveUser = async (req: Request, res: Response) => {
 
 const getUser = async (req: Request, res: Response) => {
   try {
-    const findUser = await prisma.userlist.deleteMany({
-      where : {
-        name : "Meet"
-      }
-    })
-
-    let user = await User.find();
+    const findUser = await prisma.userlist.findMany();
     res.status(201).json(findUser);
   } catch (error) {
     res.status(400).json(error);
   }
 };
 
-const getData= async (sortBy: string = '' ) => {
- const sorted = await prisma.userlist.findMany({
-  // where: {
-  //   name : "Meet"
-  // },
-  orderBy :[ {
-    name: 'desc'
-  }]
- })
-}
+const getData = async (req: Request, res: Response) => {
+  try {
+    const sortBy = req.query.sortBy;
+    type sortStatus = "asc" | "desc";
+    const sorted = await prisma.userlist.findMany({
+      orderBy: { name: sortBy as sortStatus },
+    });
+    res.status(201).json(sorted);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+};
 
 export default {
   saveUser,
   getUser,
-  getData
+  getData,
 };
--- Schema/userSchema.ts
+++ Schema/userSchema.ts
@@ -1,15 +1,15 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema(
-  {
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-  },
-  { strict: false }
-);
-userSchema.index({email:1},{unique : true})
+// const userSchema = new mongoose.Schema(
+//   {
+//     name: { type: String, required: true },
+//     email: { type: String, required: true, unique: true },
+//     password: { type: String, required: true },
+//   },
+//   { strict: false }
+// );
+// userSchema.index({email:1},{unique : true})
 
-const User = mongoose.model("User", userSchema);
+// const User = mongoose.model("User", userSchema);
 
-export default User;
+// export default User;
